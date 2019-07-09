096a3f0c23693fa67553201c0a8f06b3f31bde4b
waferreira <wagner.ferreira@omnibees.com>
2019-07-09 14:07:44 -0300

    reset

--- app/src/index.js
+++ app/src/index.js
@@ -3,4 +3,4 @@ import ReactDom from "react-dom";
 import App from "./App";
 
 
-ReactDom.render(<App /> , document.getElementById("root"));
+ReactDom.render(<App /> , document.getElementById("root")); 
