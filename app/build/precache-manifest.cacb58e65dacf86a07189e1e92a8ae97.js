4127b3066f75b4b0299439fbd076c710e66aa554
waferreira <wagner.ferreira@omnibees.com>
2019-09-20 18:55:53 -0300

    Ajustes Finais

---
+++ app/build/precache-manifest.cacb58e65dacf86a07189e1e92a8ae97.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5488f9d28cf27a506e41759158279b",
+    "url": "/NBA/index.html"
+  },
+  {
+    "revision": "9f04edbf50f40c9e55d2",
+    "url": "/NBA/static/css/main.4282783b.chunk.css"
+  },
+  {
+    "revision": "96181a219a0f657bdf0b",
+    "url": "/NBA/static/js/2.8a07bffa.chunk.js"
+  },
+  {
+    "revision": "9f04edbf50f40c9e55d2",
+    "url": "/NBA/static/js/main.1a2f5a3e.chunk.js"
+  },
+  {
+    "revision": "b9f8a22ebddd9e112c8c",
+    "url": "/NBA/static/js/runtime~main.ac4d2dc0.js"
+  },
+  {
+    "revision": "c1f17885aefc33c06accebeef0fbb3e5",
+    "url": "/NBA/static/media/background-home.c1f17885.jpg"
+  },
+  {
+    "revision": "1ac07b69e602099eb4a45c5e70525d65",
+    "url": "/NBA/static/media/background.1ac07b69.jpg"
+  },
+  {
+    "revision": "35871e289cd9b77d5d9106f74f0dc643",
+    "url": "/NBA/static/media/background.35871e28.jpg"
+  },
+  {
+    "revision": "3df206d53f39e3ef3ad88afb99d69830",
+    "url": "/NBA/static/media/background.3df206d5.jpg"
+  },
+  {
+    "revision": "7452ac0540202654c1533b8d43dd61cc",
+    "url": "/NBA/static/media/background.7452ac05.jpg"
+  },
+  {
+    "revision": "9280fdbc1c50c0a6aa3bf7989bfd4636",
+    "url": "/NBA/static/media/background.9280fdbc.jpg"
+  },
+  {
+    "revision": "c1dd70bf486d8c3c1a220bf94898d6c5",
+    "url": "/NBA/static/media/background.c1dd70bf.jpg"
+  },
+  {
+    "revision": "65c9fe3fa6deb2091eb9bb60f81921fe",
+    "url": "/NBA/static/media/bucks.65c9fe3f.png"
+  },
+  {
+    "revision": "4e7b60a02aecd13e9a184c3057e09cfa",
+    "url": "/NBA/static/media/celtics.4e7b60a0.png"
+  },
+  {
+    "revision": "1d48ab7e1a7617e4acc98d63a04fc543",
+    "url": "/NBA/static/media/fog.1d48ab7e.png"
+  },
+  {
+    "revision": "3bf8842be004d9f3c302ac26412af16b",
+    "url": "/NBA/static/media/giannis.3bf8842b.png"
+  },
+  {
+    "revision": "fc92be2eaf15985a4622ecaddcdcd533",
+    "url": "/NBA/static/media/james.fc92be2e.png"
+  },
+  {
+    "revision": "a0f5653a27d6aed4524ae25f726d24fe",
+    "url": "/NBA/static/media/kawhi.a0f5653a.png"
+  },
+  {
+    "revision": "ae27facab45245854ad376c338004431",
+    "url": "/NBA/static/media/kyrie.ae27faca.png"
+  },
+  {
+    "revision": "e125d45d955e1c223791c354fb21ea0c",
+    "url": "/NBA/static/media/lakers.e125d45d.png"
+  },
+  {
+    "revision": "f6f96b58e930d227d8c3f3d545184559",
+    "url": "/NBA/static/media/lebron-principal.f6f96b58.png"
+  },
+  {
+    "revision": "7b7281384f8dcc06947d519f5332604e",
+    "url": "/NBA/static/media/logo-nba.7b728138.png"
+  },
+  {
+    "revision": "262d3164f76ab784d0f5445496dd8984",
+    "url": "/NBA/static/media/raptrors.262d3164.png"
+  },
+  {
+    "revision": "070190cef8b786e8ef869108e6ea87f9",
+    "url": "/NBA/static/media/rockets.070190ce.png"
+  },
+  {
+    "revision": "02f1d815c23d28ff0556e4906ccbf477",
+    "url": "/NBA/static/media/stephen.02f1d815.png"
+  },
+  {
+    "revision": "c6f96ccea581a4c1bee2e91c8995c0df",
+    "url": "/NBA/static/media/warriors.c6f96cce.png"
+  }
+]);
